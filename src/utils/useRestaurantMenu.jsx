6f0ca8cdd277807e7bd57e55f29f4a44bf5d9751
Priyanshu Singh <singhpriyanshu086@gmail.com>
2025-01-03 01:41:56 +0530

    ..

--- src/utils/useRestaurantMenu.jsx
+++ src/utils/useRestaurantMenu.jsx
@@ -10,7 +10,7 @@ const useRestaurntMenu = (resId) => {
   const fetchData = async () => {
     const data = await fetch(MENU_API + resId);
     const json = await data.json();
-    setResInfo(json);
+    setResInfo(json.data);
   };
 
   return resInfo;
